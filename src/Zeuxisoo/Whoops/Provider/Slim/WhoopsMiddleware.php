b99c008b177027c582d815e9dd70c39c2f37c6cf
Zeuxis Lo <seekstudio@gmail.com>
2013-04-16 21:09:18 +0800

    supported outside on/off debug configuration

--- src/Zeuxisoo/Whoops/Provider/Slim/WhoopsMiddleware.php
+++ src/Zeuxisoo/Whoops/Provider/Slim/WhoopsMiddleware.php
@@ -9,43 +9,47 @@ use Whoops\Handler\PrettyPageHandler;
 class WhoopsMiddleware extends Middleware {
 	public function call() {
 		$app = $this->app;
-		$app->config('debug', false);
 
-		$app->config('whoops.error_page_handler', new PrettyPageHandler);
-		$app->config('whoops.slim_info_handler', function() use ($app) {
-			try {
-				$request = $app->request();
-			} catch (RuntimeException $e) {
-				return;
-			}
+		if ($app->config('debug') === true) {
+			// Switch to custom error handler by disable debug
+			$app->config('debug', false);
 
-			$app->config('whoops.error_page_handler')->addDataTable('Slim Application', array(
-				'Charset'          => $request->headers('ACCEPT_CHARSET'),
-				'Locale'           => $request->getContentCharset() ?: '<none>',
-				'Route Name'       => $app->router()->getCurrentRoute()->getName() ?: '<none>',
-				'Route Pattern'    => $app->router()->getCurrentRoute()->getPattern() ?: '<none>',
-				'Route Middleware' => $app->router()->getCurrentRoute()->getMiddleware() ?: '<none>',
-				'Application Class'=> get_class($app)
-			));
+			$app->config('whoops.error_page_handler', new PrettyPageHandler);
+			$app->config('whoops.slim_info_handler', function() use ($app) {
+				try {
+					$request = $app->request();
+				} catch (RuntimeException $e) {
+					return;
+				}
 
-			$app->config('whoops.error_page_handler')->addDataTable('Slim Application (Request)', array(
-				'URI'         => $request->getRootUri(),
-				'Request URI' => $request->getResourceUri(),
-				'Path'        => $request->getPath(),
-				'Query String'=> $request->params() ?: '<none>',
-				'HTTP Method' => $request->getMethod(),
-				'Script Name' => $request->getScriptName(),
-				'Base URL'    => $request->getUrl(),
-				'Scheme'      => $request->getScheme(),
-				'Port'        => $request->getPort(),
-				'Host'        => $request->getHost(),
-			));
-		});
+				$app->config('whoops.error_page_handler')->addDataTable('Slim Application', array(
+					'Charset'          => $request->headers('ACCEPT_CHARSET'),
+					'Locale'           => $request->getContentCharset() ?: '<none>',
+					'Route Name'       => $app->router()->getCurrentRoute()->getName() ?: '<none>',
+					'Route Pattern'    => $app->router()->getCurrentRoute()->getPattern() ?: '<none>',
+					'Route Middleware' => $app->router()->getCurrentRoute()->getMiddleware() ?: '<none>',
+					'Application Class'=> get_class($app)
+				));
 
-		$app->config('whoops', new Run);
-		$app->config('whoops')->pushHandler($app->config('whoops.error_page_handler'));
-		$app->config('whoops')->pushHandler($app->config('whoops.slim_info_handler'));
-		$app->error(array($app->config('whoops'), Run::EXCEPTION_HANDLER));
+				$app->config('whoops.error_page_handler')->addDataTable('Slim Application (Request)', array(
+					'URI'         => $request->getRootUri(),
+					'Request URI' => $request->getResourceUri(),
+					'Path'        => $request->getPath(),
+					'Query String'=> $request->params() ?: '<none>',
+					'HTTP Method' => $request->getMethod(),
+					'Script Name' => $request->getScriptName(),
+					'Base URL'    => $request->getUrl(),
+					'Scheme'      => $request->getScheme(),
+					'Port'        => $request->getPort(),
+					'Host'        => $request->getHost(),
+				));
+			});
+
+			$app->config('whoops', new Run);
+			$app->config('whoops')->pushHandler($app->config('whoops.error_page_handler'));
+			$app->config('whoops')->pushHandler($app->config('whoops.slim_info_handler'));
+			$app->error(array($app->config('whoops'), Run::EXCEPTION_HANDLER));
+		}
 
 		$this->next->call();
 	}
--- tests/index.php
+++ tests/index.php
@@ -4,6 +4,7 @@ require_once dirname(dirname(__FILE__)).'/vendor/autoload.php';
 use Slim\Slim;
 
 $app = new Slim();
+$app->config('debug', true);
 $app->add(new \Zeuxisoo\Whoops\Provider\Slim\WhoopsMiddleware);
 $app->get('/', function() use ($app) {
 	$app->urlFor('index');	// Throw exception, not defined route named 'index'
