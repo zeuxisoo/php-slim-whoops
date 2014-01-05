edb5737b9250e40dc8d65579d73151363f219613
Zeuxis Lo <seekstudio@gmail.com>
2014-01-05 20:16:07 +0800

    add .htaccess file to examples

--- examples/index.php
+++ examples/index.php
@@ -8,6 +8,6 @@ $app->config('debug', true);
 $app->config('whoops.editor', 'sublime');
 $app->add(new \Zeuxisoo\Whoops\Provider\Slim\WhoopsMiddleware);
 $app->get('/', function() use ($app) {
-	$app->urlFor('index');	// Throw exception, not defined route named 'index'
+	$app->urlFor('index');  // Throw exception, not defined route named 'index'
 });
 $app->run();
