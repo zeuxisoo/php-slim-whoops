5c118535f28cb00296da13d2039bb18a02f7fd76
Zeuxis Lo <seekstudio@gmail.com>
2014-01-14 15:12:19 +0800

    convert indentation to spaces

--- src/Zeuxisoo/Whoops/Provider/Slim/WhoopsMiddleware.php
+++ src/Zeuxisoo/Whoops/Provider/Slim/WhoopsMiddleware.php
@@ -8,82 +8,82 @@ use Whoops\Handler\PrettyPageHandler;
 use Whoops\Handler\JsonResponseHandler;
 
 class WhoopsMiddleware extends Middleware {
-	public function call() {
-		$app = $this->app;
+    public function call() {
+        $app = $this->app;
 
-		if ($app->config('debug') === true) {
-			// Switch to custom error handler by disable debug
-			$app->config('debug', false);
+        if ($app->config('debug') === true) {
+            // Switch to custom error handler by disable debug
+            $app->config('debug', false);
 
-			//
-			$app->container->singleton('whoopsPrettyPageHandler', function() {
-				return new PrettyPageHandler();
-			});
+            //
+            $app->container->singleton('whoopsPrettyPageHandler', function() {
+                return new PrettyPageHandler();
+            });
 
-			$app->container->singleton('whoopsJsonResponseHandler', function() {
-				$handler = new JsonResponseHandler();
-				$handler->onlyForAjaxRequests(true);
+            $app->container->singleton('whoopsJsonResponseHandler', function() {
+                $handler = new JsonResponseHandler();
+                $handler->onlyForAjaxRequests(true);
 
-				return $handler;
-			});
+                return $handler;
+            });
 
-			$app->whoopsSlimInfoHandler = $app->container->protect(function() use ($app) {
-				try {
-					$request = $app->request();
-				} catch (RuntimeException $e) {
-					return;
-				}
+            $app->whoopsSlimInfoHandler = $app->container->protect(function() use ($app) {
+                try {
+                    $request = $app->request();
+                } catch (RuntimeException $e) {
+                    return;
+                }
 
-				$current_route = $app->router()->getCurrentRoute();
-				$route_details = array();
+                $current_route = $app->router()->getCurrentRoute();
+                $route_details = array();
 
-				if ($current_route !== null) {
-					$route_details = array(
-						'Route Name'       => $current_route->getName() ?: '<none>',
-						'Route Pattern'    => $current_route->getPattern() ?: '<none>',
-						'Route Middleware' => $current_route->getMiddleware() ?: '<none>',
-					);
-				}
+                if ($current_route !== null) {
+                    $route_details = array(
+                        'Route Name'       => $current_route->getName() ?: '<none>',
+                        'Route Pattern'    => $current_route->getPattern() ?: '<none>',
+                        'Route Middleware' => $current_route->getMiddleware() ?: '<none>',
+                    );
+                }
 
-				$app->whoopsPrettyPageHandler->addDataTable('Slim Application', array_merge(array(
-					'Charset'          => $request->headers('ACCEPT_CHARSET'),
-					'Locale'           => $request->getContentCharset() ?: '<none>',
-					'Application Class'=> get_class($app)
-				), $route_details));
+                $app->whoopsPrettyPageHandler->addDataTable('Slim Application', array_merge(array(
+                    'Charset'          => $request->headers('ACCEPT_CHARSET'),
+                    'Locale'           => $request->getContentCharset() ?: '<none>',
+                    'Application Class'=> get_class($app)
+                ), $route_details));
 
-				$app->whoopsPrettyPageHandler->addDataTable('Slim Application (Request)', array(
-					'URI'         => $request->getRootUri(),
-					'Request URI' => $request->getResourceUri(),
-					'Path'        => $request->getPath(),
-					'Query String'=> $request->params() ?: '<none>',
-					'HTTP Method' => $request->getMethod(),
-					'Script Name' => $request->getScriptName(),
-					'Base URL'    => $request->getUrl(),
-					'Scheme'      => $request->getScheme(),
-					'Port'        => $request->getPort(),
-					'Host'        => $request->getHost(),
-				));
-			});
+                $app->whoopsPrettyPageHandler->addDataTable('Slim Application (Request)', array(
+                    'URI'         => $request->getRootUri(),
+                    'Request URI' => $request->getResourceUri(),
+                    'Path'        => $request->getPath(),
+                    'Query String'=> $request->params() ?: '<none>',
+                    'HTTP Method' => $request->getMethod(),
+                    'Script Name' => $request->getScriptName(),
+                    'Base URL'    => $request->getUrl(),
+                    'Scheme'      => $request->getScheme(),
+                    'Port'        => $request->getPort(),
+                    'Host'        => $request->getHost(),
+                ));
+            });
 
-			// Open with editor if editor is set
-			$whoops_editor = $app->config('whoops.editor');
+            // Open with editor if editor is set
+            $whoops_editor = $app->config('whoops.editor');
 
-			if ($whoops_editor !== null) {
-				$app->whoopsPrettyPageHandler->setEditor($whoops_editor);
-			}
+            if ($whoops_editor !== null) {
+                $app->whoopsPrettyPageHandler->setEditor($whoops_editor);
+            }
 
-			$app->container->singleton('whoops', function() use ($app) {
-				$run = new Run();
-				$run->pushHandler($app->whoopsPrettyPageHandler);
-				$run->pushHandler($app->whoopsJsonResponseHandler);
-				$run->pushHandler($app->whoopsSlimInfoHandler);
+            $app->container->singleton('whoops', function() use ($app) {
+                $run = new Run();
+                $run->pushHandler($app->whoopsPrettyPageHandler);
+                $run->pushHandler($app->whoopsJsonResponseHandler);
+                $run->pushHandler($app->whoopsSlimInfoHandler);
 
-				return $run;
-			});
+                return $run;
+            });
 
-			$app->error(array($app->whoops, Run::EXCEPTION_HANDLER));
-		}
+            $app->error(array($app->whoops, Run::EXCEPTION_HANDLER));
+        }
 
-		$this->next->call();
-	}
+        $this->next->call();
+    }
 }
