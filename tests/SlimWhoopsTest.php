bcc0ac79803291c7c9464fe24040492d6e666f60
Zeuxis Lo <seekstudio@gmail.com>
2015-06-15 14:04:20 +0800

    fix test case in load normal and exception can not work

--- tests/SlimWhoopsTest.php
+++ tests/SlimWhoopsTest.php
@@ -1,7 +1,20 @@
 <?php
 use Slim\App;
 use Slim\Http\Environment;
+use \Slim\Http\Uri;
+use \Slim\Http\Body;
+use \Slim\Http\Headers;
+use \Slim\Http\Request;
+use \Slim\Http\Response;
 use Zeuxisoo\Whoops\Provider\Slim\WhoopsMiddleware;
+
+class Stackable {
+    use Slim\MiddlewareAware;
+
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response) {
+        return $res->write('Center');
+    }
+}
 
 class MessageTest extends PHPUnit_Framework_TestCase {
     public function setUp() {
@@ -14,59 +27,56 @@ class MessageTest extends PHPUnit_Framework_TestCase {
 
     public function testLoadNormal() {
         $app = new App();
-        $container = $app->getContainer();
-        $container['environment'] = function () {
-            return Environment::mock([
-                'SCRIPT_NAME' => '/index.php',
-                'REQUEST_URI' => '/foo',
-                'REQUEST_METHOD' => 'GET'
-            ]);
-        };
-
-        // Set get method and response
-        $app->get('/foo', function ($req, $res, $args) {
+        $app->add(new WhoopsMiddleware);
+        $app->get('/foo', function ($req, $res) {
             $res->write('It is work');
             return $res;
         });
 
-        // Set middleware
-        $app->add(new WhoopsMiddleware);
+        $env = Environment::mock([
+            'SCRIPT_NAME' => '/index.php',
+            'REQUEST_URI' => '/foo',
+            'REQUEST_METHOD' => 'GET',
+        ]);
 
-        // Invoke app
-        ob_start();
-        $response = $app->run();
-        ob_end_clean();
+        $uri          = Uri::createFromEnvironment($env);
+        $headers      = Headers::createFromEnvironment($env);
+        $cookies      = [];
+        $serverParams = $env->all();
+        $body         = new Body(fopen('php://temp', 'r+'));
+        $req          = new Request('GET', $uri, $headers, $cookies, $serverParams, $body);
+        $res          = new Response();
 
-        $this->assertEquals(200, $response->getStatusCode());
-        $this->assertEquals('It is work', (string) $response->getBody());
+        $resOut = $app($req, $res);
+
+        $this->assertInstanceOf('\Psr\Http\Message\ResponseInterface', $resOut);
+        $this->assertEquals('It is work', (string)$res->getBody());
     }
 
     public function testException() {
         $app = new App();
-        $container = $app->getContainer();
-        $container['environment'] = function () {
-            return Environment::mock([
-                'SCRIPT_NAME' => '/index.php',
-                'REQUEST_URI' => '/foo',
-                'REQUEST_METHOD' => 'GET'
-            ]);
-        };
-
-        $app->get('/foo', function ($req, $res, $args) {
-            throw new \Exception('Test Message', 100);
-            return $res;
+        $app->add(new WhoopsMiddleware);
+        $app->get('/foo', function ($req, $res) use ($app) {
+            return $app->router->urlFor('index');
         });
 
-        // Set middleware
-        $app->add(new WhoopsMiddleware);
+        $env = Environment::mock([
+            'SCRIPT_NAME' => '/index.php',
+            'REQUEST_URI' => '/foo',
+            'REQUEST_METHOD' => 'GET',
+        ]);
 
-        // Invoke app
-        ob_start();
-        $response = $app->run();
-        ob_end_clean();
+        $uri          = Uri::createFromEnvironment($env);
+        $headers      = Headers::createFromEnvironment($env);
+        $cookies      = [];
+        $serverParams = $env->all();
+        $body         = new Body(fopen('php://temp', 'r+'));
+        $req          = new Request('GET', $uri, $headers, $cookies, $serverParams, $body);
+        $res          = new Response();
 
-        $this->assertEquals(500, $response->getStatusCode());
-        $this->assertContains('Test Message', (string) $response->getBody());
+        $this->setExpectedException('\RuntimeException');
+
+        $app($req, $res);
     }
 
     public function testMiddlewareIsWorkingAndEditorIsSet() {
