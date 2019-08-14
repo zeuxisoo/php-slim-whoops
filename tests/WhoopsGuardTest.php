894ba0d93d3ab26d20ea2637a57a612b3f62dc57
Zeuxis Lo <seekstudio@gmail.com>
2019-08-14 15:35:52 +0800

    add whoops guard test case

---
+++ tests/WhoopsGuardTest.php
@@ -0,0 +1,73 @@
+<?php
+namespace Zeuxisoo\Whoops\Slim\Tests;
+
+use Slim\Psr7\Factory\ServerRequestFactory;
+use Zeuxisoo\Whoops\Slim\WhoopsGuard;
+use Whoops\Run as WhoopsRun;
+
+class WhoopsGuardTest extends TestCase {
+
+    public function testShouldReturnWhoops() {
+        $request = (new ServerRequestFactory)->createServerRequest("GET", "http://example.com/");
+
+        $guard = new WhoopsGuard();
+        $guard->setRequest($request);
+
+        $whoops = $guard->install();
+
+        $this->assertInstanceOf(WhoopsRun::class, $whoops);
+    }
+
+    public function testSetCustomHandlers() {
+        $request = (new ServerRequestFactory)->createServerRequest("GET", "http://example.com/");
+
+        $guard = new WhoopsGuard();
+        $guard->setRequest($request);
+        $guard->setHandlers([
+            function($exception, $inspector, $run) {
+                $message = $exception->getMessage();
+                $title   = $inspector->getExceptionName();
+
+                echo "{$title} -> {$message}";
+                exit;
+            }
+        ]);
+
+        $whoops = $guard->install();
+
+        $this->assertInstanceOf(WhoopsRun::class, $whoops);
+
+        // Current handlers: prettyPageHandler, customHandler
+        $this->assertEquals(2, count($whoops->getHandlers()));
+    }
+
+    public function testSetEditor() {
+        $request = (new ServerRequestFactory)->createServerRequest("GET", "http://example.com/");
+
+        $guard = new WhoopsGuard([ 'editor' => 'sublime', ]);
+        $guard->setRequest($request);
+
+        $whoops = $guard->install();
+
+        $prettyPageHandler = $whoops->getHandlers()[0];
+
+        $this->assertEquals(
+            $prettyPageHandler->getEditorHref('/foo/bar.php', 10),
+            'subl://open?url=file://%2Ffoo%2Fbar.php&line=10'
+        );
+    }
+
+    public function testPageTitle() {
+        $request = (new ServerRequestFactory)->createServerRequest("GET", "http://example.com/");
+
+        $guard = new WhoopsGuard([ 'title' => 'Hello World' ]);
+        $guard->setRequest($request);
+
+        $whoops = $guard->install();
+
+        $prettyPageHandler = $whoops->getHandlers()[0];
+
+        $this->assertEquals($prettyPageHandler->getPagetitle(), 'Hello World');
+    }
+
+}
