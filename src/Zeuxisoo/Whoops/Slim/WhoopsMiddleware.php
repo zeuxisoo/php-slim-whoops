779d8186cd537e73bc16c33e1045b0db3e478073
Zeuxis Lo <seekstudio@gmail.com>
2019-08-15 18:01:55 +0800

    add missed comment to whoops middleware

--- src/Zeuxisoo/Whoops/Slim/WhoopsMiddleware.php
+++ src/Zeuxisoo/Whoops/Slim/WhoopsMiddleware.php
@@ -13,11 +13,24 @@ class WhoopsMiddleware implements MiddlewareInterface {
     protected $settings = [];
     protected $handlers = [];
 
+    /**
+     * Instance the whoops middleware object
+     *
+     * @param array $settings
+     * @param array $handlers
+     */
     public function __construct(array $settings = [], array $handlers = []) {
         $this->settings = $settings;
         $this->handlers = $handlers;
     }
 
+    /**
+     * Handle the requests
+     *
+     * @param ServerRequestInterface $request
+     * @param RequestHandlerInterface $handler
+     * @return ResponseInterface
+     */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface {
         $whoopsGuard = new WhoopsGuard($this->settings);
         $whoopsGuard->setRequest($request);
